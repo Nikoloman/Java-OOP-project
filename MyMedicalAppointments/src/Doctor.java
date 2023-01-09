8b6480a4e65e5631ef44cff7ee550a98a7432cac
Nikoloman <hectorvel52@gmail.com>
2023-01-09 12:35:54 -0600

    A new project about a hospital and it's appointments

---
+++ MyMedicalAppointments/src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    //Properties
+    int id;
+    String name;
+    String speciality;
+
+    //Methods
+    public void showName(){
+        System.out.println("The name of the doctor is: " + name);
+    }
+}
---
+++ MyMedicalAppointments/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Doctor doctorJesus = new Doctor();
+        doctorJesus.name = "Jesus Velazquez";
+        doctorJesus.showName();
+    }
+}
