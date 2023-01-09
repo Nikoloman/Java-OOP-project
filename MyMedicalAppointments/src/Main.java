a7bb6b0bacf84a6a44fce49b6d3c2175cc8dae21
Nikoloman <hectorvel52@gmail.com>
2023-01-09 15:02:32 -0600

    Declaring a constant using "final"

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -2,17 +2,8 @@ import UI.UIMenus;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor doctorJesus = new Doctor("Jesus Orozco"); //Passing the name just as reference to the constructor
-        doctorJesus.showName(); //Gives null because it was never really assigned to the object
-        doctorJesus.name = "Jesus Velazquez"; //Assigning the name properly to the object
-        doctorJesus.showName(); //Gives the name of the object
-        doctorJesus.showIdDoctor();
-        System.out.println(Doctor.id);
-
-        Doctor doctorMax = new Doctor();
-        doctorMax.showIdDoctor();
-        System.out.println(Doctor.id);
 
         UIMenus.showMenu();
+
     }
 }
--- MyMedicalAppointments/src/UI/UIMenus.java
+++ MyMedicalAppointments/src/UI/UIMenus.java
@@ -3,6 +3,9 @@ package UI;
 import java.util.Scanner;
 
 public class UIMenus {
+    
+    public static final String[] MONTHS = {"January", "February", "March", "April", "May", "June",
+                                    "July", "August", "September", "October", "November", "December"};
 
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
@@ -12,25 +15,19 @@ public class UIMenus {
         do {
             System.out.println("1. Doctor");
             System.out.println("2. Patient");
-            System.out.println("0. Salir");
+            System.out.println("0. Exit");
 
             Scanner sc = new Scanner(System.in);
-            response = Integer.valueOf(sc.nextLine());
+            response = sc.nextInt();
 
-            switch (response){
-                case 1:
-                    System.out.println("Doctor");
-                    break;
-                case 2:
+            switch (response) {
+                case 1 -> System.out.println("Doctor");
+                case 2 -> {
                     response = 0;
                     showPatientMenu();
-
-                    break;
-                case 0:
-                    System.out.println("Thank you for you visit");
-                    break;
-                default:
-                    System.out.println("Please select a correct answer");
+                }
+                case 0 -> System.out.println("Thank you for you visit");
+                default -> System.out.println("Please select a correct answer");
             }
         }while (response != 0);
     }
@@ -38,25 +35,24 @@ public class UIMenus {
     static void showPatientMenu(){
         int response = 0;
         do {
-            System.out.println("\n\n");
+            System.out.println("\n");
             System.out.println("Patient");
             System.out.println("1. Book an appointment");
             System.out.println("2. My appointments");
             System.out.println("0. Return");
 
             Scanner sc = new Scanner(System.in);
-            response = Integer.valueOf(sc.nextLine());
+            response = sc.nextInt();
 
-            switch (response){
-                case 1:
-                    System.out.println("::Book an appointment");
-                    break;
-                case 2:
-                    System.out.println("::My appointments");
-                    break;
-                case 0:
-                    showMenu();
-                    break;
+            switch (response) {
+                case 1 -> {
+                    System.out.println("----- Book an appointment -----");
+                    for (int i = 1; i < 4; i++) {
+                        System.out.println(i + ".- " + MONTHS[i]);
+                    }
+                }
+                case 2 -> System.out.println("----- My appointments -----");
+                case 0 -> showMenu();
             }
         }while (response != 0);
     }
