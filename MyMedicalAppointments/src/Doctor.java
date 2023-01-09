0434af6a1f95461b9fd48f2f7bd835573592fd36
Nikoloman <hectorvel52@gmail.com>
2023-01-09 13:17:11 -0600

    Creating constructors and using Java Docs (fancy, isn't it?)

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -4,6 +4,19 @@ public class Doctor {
     String name;
     String speciality;
 
+    //Constructor
+    Doctor(){
+        System.out.println("The object Doctor has been constructed");
+    }
+
+    /**
+     *
+     * @param name The name of the doctor
+     */
+    Doctor(String name){
+        System.out.println("The assigned name is: " + name);
+    }
+
     //Methods
     public void showName(){
         System.out.println("The name of the doctor is: " + name);
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor doctorJesus = new Doctor();
-        doctorJesus.name = "Jesus Velazquez";
-        doctorJesus.showName();
+        Doctor doctorJesus = new Doctor("Jesus Orozco"); //Passing the name just as reference to the constructor
+        doctorJesus.showName(); //Gives null because it was never really assigned to the object
+        doctorJesus.name = "Jesus Velazquez"; //Assigning the name properly to the object
+        doctorJesus.showName(); //Gives the name of the object
     }
 }
